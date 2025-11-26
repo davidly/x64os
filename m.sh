d433fe7c3436c790bdb19e7770c8c48c50f6bce4
davidly <davidly_lee@hotmail.com>
2025-11-26 07:46:50 -0800

    cleanup

--- m.sh
+++ m.sh
@@ -1,4 +1,4 @@
 g++ -DX64OS -fcf-protection=none -U_FORTIFY_SOURCE -O2 -Wno-psabi -Wno-stringop-overflow -Wno-format-security -fsigned-char -fno-builtin -I . x64os.cxx x64.cxx -o x64os -static
-cp x64os /mnt/c/users/david/onedrive/x64os/bin
-cp x64os bin
+# cp x64os /mnt/c/users/david/onedrive/x64os/bin
+# cp x64os bin
 objdump -d x64os >x64os.txt
