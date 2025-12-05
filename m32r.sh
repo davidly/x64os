bc530b4ce4c3f2165b29dc4ee661831eaa430dff
davidly <davidly_lee@hotmail.com>
2025-12-05 08:13:46 -0800

    add release build script for x32os on Linux

---
+++ m32r.sh
@@ -0,0 +1,4 @@
+g++ -DNDEBUG -DX32OS -m32 -fcf-protection=none -U_FORTIFY_SOURCE -O2 -Wno-psabi -Wno-stringop-overflow -Wno-format-security -fsigned-char -fno-builtin -I . x64os.cxx x64.cxx -o x32os -static
+#cp x32os /mnt/c/users/david/onedrive/x64os/bin
+#cp x32os bin
+#objdump -d x32os >x32os.txt
