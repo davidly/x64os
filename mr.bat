ede811c2dacbe9e507e3fcd5e8f5793bff3366cb
davidly <davidly_lee@hotmail.com>
2026-02-11 13:55:23 -0800

    make the endianness swap check at compile-time, not runtime.

--- mr.bat
+++ mr.bat
@@ -1,7 +1,7 @@
 @echo off
 del *.pdb 2>nul
 rem cl /DX64OS /W4 /wd4201 /wd4996 /nologo x64os.cxx x64.cxx /I. /EHsc /DNDEBUG /GS- /GL /Ot /Ox /Ob3 /Oi /Qpar /Zi /Fa /FAs /link /OPT:REF user32.lib /DEBUG:FASTLINK
-cl /DX64OS /W4 /wd4201 /wd4996 /nologo x64os.cxx x64.cxx /I. /EHsc /DNDEBUG /GS- /GL /Ot /Ox /Ob3 /Oi /Qpar /Zi /link /OPT:REF user32.lib /DEBUG:FASTLINK
+cl /DX64OS /W4 /wd4201 /wd4996 /wd4127 /nologo x64os.cxx x64.cxx /I. /EHsc /DNDEBUG /GS- /GL /Ot /Ox /Ob3 /Oi /Qpar /Zi /link /OPT:REF user32.lib /DEBUG:FASTLINK
 
 
 
