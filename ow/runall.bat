f083756c80afcb5e40041d581e21a1cab1b3d153
davidly <davidly_lee@hotmail.com>
2026-04-12 11:46:11 -0700

    cleanup test scripts

--- ow/runall.bat
+++ ow/runall.bat
@@ -2,63 +2,39 @@
 setlocal
 
 set outputfile=runall_test.txt
-echo %date% %time% >%outputfile%
+echo %date% %time% >"%outputfile%"
 
 rem FAILED tests: mm, tao, ttypes, nantst, tatomic, tregex (watcom can't compile it)
-rem               ff, an, ba all fail in compilation, have missing code, or with bad code at execution time
+rem               an, ba fail in compilation, have missing code, or with bad code at execution time
 
 set _applist=tcmp t e printint sieve simple tmuldiv tpi ts tarray tbits trw trw2 ^
              tstr fileops ttime tm glob tap tsimplef tphi tf ttt td terrno ^
              t_setjmp tex pis lenum tdir ^
              trename nqueens fopentst fact triangle mm_old hidave ^
-             termiosf mandle tmmap
+             termiosf mandle tmmap ff targs tgets
 
 ( for %%a in (%_applist%) do (
     echo %%a
-    echo %%a>>%outputfile%
-    copy ..\c_tests\%%a.c . > NUL 2>&1
+    echo %%a>>"%outputfile%"
+    copy ..\c_tests\%%a.c . >NUL 2>&1
     call mn.bat %%a
-    ..\x32os %%a.elf >>%outputfile%
-    del %%a.c > NUL 2>&1
-    del %%a.o > NUL 2>&1
-    del %%a.map > NUL 2>&1
-    del %%a.elf > NUL 2>&1
+
+    if "%%a"=="ff" (
+        ..\x32os %%a.elf . %%a.c >>"%outputfile%"
+    ) else if "%%a"=="targs" (
+        ..\x32os "-e:solo=iu;group=i-dle" %%a.elf a bb ccc dddd >>"%outputfile%"
+    ) else if "%%a"=="tgets" (
+        ..\x32os %%a.elf <..\c_tests\tgets.txt >>"%outputfile%"
+    ) else (
+        ..\x32os %%a.elf >>"%outputfile%"
+    )
+
+    del %%a.c >NUL 2>&1
+    del %%a.o >NUL 2>&1
+    del %%a.map >NUL 2>&1
+    del %%a.elf >NUL 2>&1
 ) )
 
-rem ff test
-echo ff
-echo ff>>%outputfile%
-copy ..\c_tests\ff.c . > NUL 2>&1
-call mn.bat ff
-..\x32os ff.elf . ff.c>>%outputfile%
-del ff.c > NUL 2>&1
-del ff.o > NUL 2>&1
-del ff.map > NUL 2>&1
-del ff.elf > NUL 2>&1
-
-rem targs test
-echo targs
-echo targs>>%outputfile%
-copy ..\c_tests\targs.c . > NUL 2>&1
-call mn.bat targs
-..\x32os -e:solo=iu;group=i-dle targs.elf a bb ccc dddd >>%outputfile%
-del targs.c > NUL 2>&1
-del targs.o > NUL 2>&1
-del targs.map > NUL 2>&1
-del targs.elf > NUL 2>&1
-
-rem tgets test
-echo tgets
-echo tgets>>%outputfile%
-copy ..\c_tests\tgets.c . > NUL 2>&1
-call mn.bat tgets
-..\x32os tgets.elf <..\c_tests\tgets.txt >>%outputfile%
-del tgets.c > NUL 2>&1
-del tgets.o > NUL 2>&1
-del tgets.map > NUL 2>&1
-del tgets.elf > NUL 2>&1
-
-echo %date% %time% >>%outputfile%
-dos2unix %outputfile% > NUL 2>&1
-diff -b baseline_%outputfile% %outputfile%
-
+echo %date% %time% >>"%outputfile%"
+dos2unix "%outputfile%" >NUL 2>&1
+diff -b "baseline_%outputfile%" "%outputfile%"
--- ow/runall.sh
+++ ow/runall.sh
@@ -2,58 +2,32 @@
 
 outputfile="runall_test.txt"
 date_time=$(date)
-echo "$date_time" >$outputfile
+echo "$date_time" >"$outputfile"
 
 for arg in tcmp t e printint sieve simple tmuldiv tpi ts tarray tbits trw trw2 \
            tstr fileops ttime tm glob tap tsimplef tphi tf ttt td terrno \
            t_setjmp tex pis lenum tdir \
            trename nqueens fopentst fact triangle mm_old hidave \
-           termiosf mandle tmmap;
+           termiosf mandle tmmap ff targs tgets
 do
-    echo $arg
-    echo $arg >>$outputfile
-    cp ../c_tests/$arg.c .
-    ./mn.sh $arg
-    ../x32os $arg.elf >>$outputfile
-    rm $arg.c > /dev/null 2>&1
-    rm $arg.o > /dev/null 2>&1
-    rm $arg.map > /dev/null 2>&1
-    rm $arg.elf > /dev/null 2>&1
+    echo "$arg"
+    echo "$arg" >>"$outputfile"
+    cp "../c_tests/$arg.c" .
+    ./mn.sh "$arg"
+
+    if [ "$arg" = "ff" ]; then
+        ../x32os "$arg.elf" . "$arg.c" >>"$outputfile"
+    elif [ "$arg" = "targs" ]; then
+        ../x32os "-e:solo=iu;group=i-dle" "$arg.elf" a bb ccc dddd >>"$outputfile"
+    elif [ "$arg" = "tgets" ]; then
+        ../x32os "$arg.elf" <../c_tests/tgets.txt >>"$outputfile"
+    else
+        ../x32os "$arg.elf" >>"$outputfile"
+    fi
+
+    rm -f "$arg.c" "$arg.o" "$arg.map" "$arg.elf"
 done
 
-# ff test
-echo ff
-echo ff >>$outputfile
-cp ../c_tests/ff.c .
-./mn.sh ff
-../x32os ff.elf . ff.c >>$outputfile
-rm ff.c > /dev/null 2>&1
-rm ff.o > /dev/null 2>&1
-rm ff.map > /dev/null 2>&1
-rm ff.elf > /dev/null 2>&1
-
-# targs test
-echo targs
-echo targs >>$outputfile
-cp ../c_tests/targs.c .
-./mn.sh targs
-../x32os "-e:solo=iu;group=i-dle" targs.elf a bb ccc dddd >>$outputfile
-rm targs.c > /dev/null 2>&1
-rm targs.o > /dev/null 2>&1
-rm targs.map > /dev/null 2>&1
-rm targs.elf > /dev/null 2>&1
-
-# tgets test
-echo tgets
-echo tgets >>$outputfile
-cp ../c_tests/tgets.c .
-./mn.sh tgets
-../x32os tgets.elf <../c_tests/tgets.txt >>$outputfile
-rm tgets.c > /dev/null 2>&1
-rm tgets.o > /dev/null 2>&1
-rm tgets.map > /dev/null 2>&1
-rm tgets.elf > /dev/null 2>&1
-
 date_time=$(date)
-echo "$date_time" >>$outputfile
-diff baseline_$outputfile $outputfile
+echo "$date_time" >>"$outputfile"
+diff "baseline_$outputfile" "$outputfile"
