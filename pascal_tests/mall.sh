f478d3636a5ea2471fcc9c7c8bec3c05aa683fba
davidly <davidly_lee@hotmail.com>
2026-05-07 19:07:22 -0700

    build all the new test cases

--- pascal_tests/mall.sh
+++ pascal_tests/mall.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-apps=(sieve e ttt mm chk tap tphi nqueens nq1d)
+apps=(sieve e ttt mm chk tap tphi nqueens nq1d trw mandle tf tdir tcmp tstr \
+      trename sleeptm tmuldiv lenum tpi pis tshift ff)
 
 for arg in ${apps[@]}
 do
